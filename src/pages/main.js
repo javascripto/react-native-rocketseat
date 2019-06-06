146773713cfee72f0bcd5a5f0abdb1bf0dbe974d
Yuri Alves <yurialves2@gmail.com>
2019-06-06 19:09:04 -0300

    NavigationOptions title

--- src/pages/main.js
+++ src/pages/main.js
@@ -2,6 +2,11 @@ import React, { Component } from 'react';
 import { Text, View } from 'react-native';
 
 export default class Main extends Component {
+
+  static navigationOptions = {
+    title: 'JSHunt',
+  }
+
   render() {
     return (
       <View>
