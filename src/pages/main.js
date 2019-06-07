be6ad9d76838299f3c2d7be8650b9fe90f1ec03c
Yuri Alves <yurialves2@gmail.com>
2019-06-07 14:15:30 -0300

    listing products

--- src/pages/main.js
+++ src/pages/main.js
@@ -1,5 +1,5 @@
 import React, { Component } from 'react';
-import { Text, View } from 'react-native';
+import { Text, View, FlatList, Button, TouchableOpacity } from 'react-native';
 
 import api from '../services/api';
 
@@ -9,6 +9,10 @@ export default class Main extends Component {
     title: 'JSHunt'
   }
 
+  state = {
+    products: []
+  }
+
   componentDidMount() {
     this.loadProducts();
   }
@@ -16,17 +20,34 @@ export default class Main extends Component {
   loadProducts = async () => {
     const response = await api.get('/products');
 
-    const { docs } = response.data;
+    const { docs: products } = response.data;
 
-    console.log(docs); // (CTRL + M) no emulador para debugar remotamente
+    console.log(products); // (CTRL + M) no emulador para debugar remotamente
+
+    this.setState({ products })
   }
 
+  renderItems = ({ item: { title, description, url } }) => (
+    <View>
+      <Text>{title}</Text>
+      <Text>{description}</Text>
+      <TouchableOpacity onPress={() => {}}>
+        <Text>Acessar</Text>
+      </TouchableOpacity>
+    </View>
+  );
+
+
   render() {
+    const { products } = this.state;
     return (
       <View>
-        <Text>Página Main</Text>
+        <FlatList
+          data={products}
+          keyExtractor={p => p._id}
+          renderItem={this.renderItems}>
+        </FlatList>
       </View>
     );
-
   }
 }
